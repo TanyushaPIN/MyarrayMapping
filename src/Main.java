9e72fa2db5058f62b2abeec215c80bf4e3bcefa8
Viloria999 <comiksans999@mail.ru>
2026-09-22 11:37:11 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class Main {
@@ -10,7 +13,25 @@ public class Main {
         }
         return result;
     }
+//--------------------------------------------------------------------------------------------------------
+    public static Map<Integer, Integer> createMap(Integer[] arr){
+        Map<Integer,Integer> myMap = new HashMap<>();
+        for (Integer x : arr) {
+            if (myMap.containsKey(x)) {
+                int tmp = myMap.get(x) + 1;
+                myMap.put(x,tmp);
+            }
+            else {
+                myMap.put(x,1);
+            }
+        }
+        return myMap;
+    }
     public static void main(String[] args) {
+        //Напишите метод arrayMapping(), который принимает на вход массив любого типа, вторым арументом метод должен принимать класс,
+        // реализующий интерфейс Function, в котором один параметризованный метод - T apply(T o).
+        //
+        //Метод должен быть реализован так чтобы возвращать новый массив, к каждому элементу которого была применена функция apply
         Integer[] numbers = {1, 2, 3};
         Integer [] result = arrayMapping(numbers, x -> x * 3);
         System.out.println(Arrays.toString(result));
@@ -22,5 +43,10 @@ public class Main {
         Double[] numbersD = {1.5, 2.3, 3.0};
         Double [] resultD = arrayMapping(numbersD, x -> x * 1.5);
         System.out.println(Arrays.toString(resultD));
+        //---------------------------------------------------------------------------
+        //Напишите метод, который получает на вход массив элементов и возвращает Map ключи в котором - элементы,
+        // а значения - сколько раз встретился этот элемент
+        Integer[] arr = {1,2,1,1,4,7,7,4};
+        System.out.println(createMap(arr));
     }
 }
